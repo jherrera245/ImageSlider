1de16eb9bfa19902f08f944dca3af100c0482d5d
jherrera245 <245jh17@gmail.com>
2023-01-31 16:13:39 -0600

    Añadiendo funcionalidad de Slider y Boton

--- app/src/main/java/com/jherrera/imageslider/MainActivity.java
+++ app/src/main/java/com/jherrera/imageslider/MainActivity.java
@@ -2,13 +2,56 @@ package com.jherrera.imageslider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrirNavegador;
+    private SliderView sliderView;
+    private final int[] IMAGES = {
+            R.drawable.image_1,
+            R.drawable.image_2,
+            R.drawable.image_3,
+            R.drawable.image_4,
+            R.drawable.image_5,
+            R.drawable.image_6
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setInitSlider();
+        setInitButtons();
+    }
+
+    private void setInitButtons() {
+        buttonAbrirNavegador = findViewById(R.id.buttonAbrirNavegador);
+
+        buttonAbrirNavegador.setOnClickListener(v -> {
+            String link = "https://play.google.com/store/movies/details/Yo_Robot?id=rlOHyl07F4E&hl=es_NI";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(link));
+            startActivity(intent);
+        });
+    }
+
+    private void setInitSlider() {
+        //Inicializar componenstes
+        sliderView = findViewById(R.id.imageSlider);
+        SliderAdapter sliderAdapter = new SliderAdapter(IMAGES);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
     }
 }
